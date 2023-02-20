5b8a836907dfa027c7ee5800d2d4683c6211640b
Zhanna93 <121199582+Zhanna93@users.noreply.github.com>
2023-02-20 15:32:49 +0200

    mobile

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,12 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+ function toggleModal() {
+   const isModalOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
+   refs.modal.classList.toggle("is-hidden");
+   
+    const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   }
 })();
